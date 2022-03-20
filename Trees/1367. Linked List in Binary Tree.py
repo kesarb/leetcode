b4f4f503c4db0d967a05eb3c70bdcff0f61102c5
kesarb <venkat.bavandla@gmail.com>
2022-03-20 00:05:54 -0700

    Create 1367. Linked List in Binary Tree.py

---
+++ Trees/1367. Linked List in Binary Tree.py
@@ -0,0 +1,88 @@
+"""
+1367. Linked List in Binary Tree
+Medium
+
+1307
+
+47
+
+Add to List
+
+Share
+Given a binary tree root and a linked list with head as the first node. 
+
+Return True if all the elements in the linked list starting from the head correspond to some downward path connected in the binary tree otherwise return False.
+
+In this context downward path means a path that starts at some node and goes downwards.
+
+ 
+
+Example 1:
+
+
+
+Input: head = [4,2,8], root = [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+Output: true
+Explanation: Nodes in blue form a subpath in the binary Tree.  
+Example 2:
+
+
+
+Input: head = [1,4,2,6], root = [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+Output: true
+Example 3:
+
+Input: head = [1,4,2,6,8], root = [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+Output: false
+Explanation: There is no path in the binary tree that contains all the elements of the linked list from head.
+ 
+
+Constraints:
+
+The number of nodes in the tree will be in the range [1, 2500].
+The number of nodes in the list will be in the range [1, 100].
+1 <= Node.val <= 100 for each node in the linked list and binary tree.
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+        if root == None and head:
+            return False
+        if head == None:
+            return True
+        
+        def rec(head, root):
+            if head == None:
+                return True
+            
+            if root == None and head:
+                return False
+            
+            if head.val == root.val:
+                return rec(head.next, root.left) or rec(head.next, root.right)
+            return False
+        
+        def getNode(curr):
+            if curr == None:
+                return False
+            if curr.val == head.val:
+                if rec(head, curr):
+                    return True
+            return getNode(curr.left) or getNode(curr.right)
+        
+        
+        return getNode(root)
+            
+        return False
+    
