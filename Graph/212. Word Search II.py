5e30c30fa8f604ac58d8eeed64b09872752b4b16
kesarb <venkat.bavandla@gmail.com>
2022-03-24 19:16:50 -0700

    Update 212. Word Search II.py

--- Graph/212. Word Search II.py
+++ Graph/212. Word Search II.py
@@ -37,88 +37,90 @@ class Node():
     def __init__(self):
         self.childrens = {}
         self.isWord = False
-        
+
+
 class Trie():
     def __init__(self):
         self.root = Node()
-    
+
     def addWord(self, word):
         if not word:
             return
         curr = self.root
-        
+
         for each in word:
             if each not in curr.childrens:
-                curr.childrens[each]=Node()
+                curr.childrens[each] = Node()
             curr = curr.childrens[each]
         curr.isWord = True
-    
+        curr.word = word
+        
+
     def print(self):
         curr = self.root
-        
-        def rec(curr,s):
+
+        def rec(curr, s):
             if not curr:
-                print ("Somthing is missing")
+                print("Somthing is missing")
             if curr.isWord:
                 print(s)
             for each in curr.childrens:
-                rec(curr.childrens[each], s+each)
+                rec(curr.childrens[each], s + each)
+
         rec(curr, "")
-    
-    
-        
-    
+
+
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         # construct Trie
         #   TreeBuild(O(n*maxlen(word))
         #   Expose the print and get Node all childrens
-        
+
         # take each cell and find whether will get word or not using dfs prcodudre
         # let say cell contains 'o', get the all the node starts with 'o' position (i,j)
         # even if its word expolore till completing the dfs untill no words found
         m = len(board)
         n = len(board[0])
-        
+
         trie = Trie()
         for each in words:
             trie.addWord(each)
-        
+
         root = trie.root
         res = set()
-        
-        visit = set()
-        neighbours = [(0,1), (0, -1), (1,0), (-1, 0)]
-        
+
+        neighbours = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
         @cache
-        def isSafe(i,j):
-            if 0<=i<m and 0<=j<n:
+        def isSafe(i, j):
+            if 0 <= i < m and 0 <= j < n:
                 return True
             return False
-        
-        def dfs(i,j, visit, s, node):
-            if not node or (i,j) in visit:
+
+        def dfs(i, j, node):
+            if not node or board[i][j]=='#':
                 return ""
-            
+
             if node.isWord == True:
-                res.add(s)
-                
-            visit.add((i,j))
-            
+                res.add(node.word)
+            letter = board[i][j]
+            board[i][j] = '#'
+
             for u, v in neighbours:
-                u1 = i+u
-                v1 = j+v
-            
-                if isSafe(u1,v1) and (u1,v1) not in visit:
+                u1 = i + u
+                v1 = j + v
+
+                if isSafe(u1, v1) and board[u1][v1]!='#':
                     c = board[u1][v1]
                     if c in node.childrens:
-                        dfs(u1,v1, visit, s+c, node.childrens[c])
+                        dfs(u1, v1, node.childrens[c])
                         
-            visit.remove((i,j))
-            
+            board[i][j]=letter
+
         for i in range(m):
             for j in range(n):
                 if board[i][j] in root.childrens:
-                    dfs(i,j, visit, board[i][j], root.childrens[board[i][j]])
-                
+                    dfs(i, j, root.childrens[board[i][j]])
+
         return list(res)
+
