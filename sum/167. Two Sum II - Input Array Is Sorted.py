1a88db784c41e062ffd78969f620f23b03a098c8
kesarb <venkat.bavandla@gmail.com>
2022-05-27 14:08:38 -0700

    Create 167. Two Sum II - Input Array Is Sorted.py

---
+++ sum/167. Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,22 @@
+"""
+167. Two Sum II - Input Array Is Sorted
+
+"""
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+        
+        i = 0
+        j = len(numbers)-1
+        while i < j:
+            if numbers[i] + numbers[j] == target:
+                return [i+1, j+1]
+            elif numbers[i] + numbers[j] < target:
+                i+=1
+            else:
+                j-=1
+
+        return []
+            
+        
