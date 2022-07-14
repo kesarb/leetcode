1a336f145e4d39a75efea68a668f521b953434d3
kesarb <venkat.bavandla@gmail.com>
2022-07-14 12:31:45 -0700

    Create 2128. Remove All Ones With Row and Column Flips.py

---
+++ matrix/2128. Remove All Ones With Row and Column Flips.py
@@ -0,0 +1,64 @@
+
+"""
+2128. Remove All Ones With Row and Column Flips
+2128. Remove All Ones With Row and Column Flips
+Medium
+
+You are given an m x n binary matrix grid.
+
+In one operation, you can choose any row or column and flip each value in that row or column (i.e., changing all 0's to 1's, and all 1's to 0's).
+
+Return true if it is possible to remove all 1's from grid using any number of operations or false otherwise.
+
+ 
+
+Example 1:
+
+Input: grid = [[0,1,0],[1,0,1],[0,1,0]]
+Output: true
+Explanation: One possible way to remove all 1's from grid is to:
+- Flip the middle row
+- Flip the middle column
+
+Example 2:
+
+Input: grid = [[1,1,0],[0,0,0],[0,0,0]]
+Output: false
+Explanation: It is impossible to remove all 1's from grid.
+
+Example 3:
+
+Input: grid = [[0]]
+Output: true
+Explanation: There are no 1's in grid.
+
+ 
+
+Constraints:
+
+    m == grid.length
+    n == grid[i].length
+    1 <= m, n <= 300
+    grid[i][j] is either 0 or 1.
+
+
+"""
+
+class Solution:
+    def removeOnes(self, grid: List[List[int]]) -> bool:
+        for col in range(len(grid[0])):
+            if grid[0][col]:
+                for row in range(len(grid)):
+                    grid[row][col] ^= 1
+        
+        for row in range(1, len(grid)):
+            ctr = 0
+            for col in range(len(grid[0])):
+                ctr += grid[row][col]
+            
+            if ctr != 0 and ctr != len(grid[0]):
+                return False
+        
+        return True
+
+        
