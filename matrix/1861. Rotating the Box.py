9f11acb894029191d197d0cc043e66cfd8a99ad7
kesarb <venkat.bavandla@gmail.com>
2022-09-27 01:21:46 -0700

    Create 1861. Rotating the Box.py

---
+++ matrix/1861. Rotating the Box.py
@@ -0,0 +1,86 @@
+"""
+1861. Rotating the Box
+
+
+1861. Rotating the Box
+Medium
+
+690
+
+44
+
+Add to List
+
+Share
+You are given an m x n matrix of characters box representing a side-view of a box. Each cell of the box is one of the following:
+
+A stone '#'
+A stationary obstacle '*'
+Empty '.'
+The box is rotated 90 degrees clockwise, causing some of the stones to fall due to gravity. Each stone falls down until it lands on an obstacle, another stone, or the bottom of the box. Gravity does not affect the obstacles' positions, and the inertia from the box's rotation does not affect the stones' horizontal positions.
+
+It is guaranteed that each stone in box rests on an obstacle, another stone, or the bottom of the box.
+
+Return an n x m matrix representing the box after the rotation described above.
+
+ 
+
+Example 1:
+
+
+
+Input: box = [["#",".","#"]]
+Output: [["."],
+         ["#"],
+         ["#"]]
+Example 2:
+
+
+
+Input: box = [["#",".","*","."],
+              ["#","#","*","."]]
+Output: [["#","."],
+         ["#","#"],
+         ["*","*"],
+         [".","."]]
+Example 3:
+
+
+
+Input: box = [["#","#","*",".","*","."],
+              ["#","#","#","*",".","."],
+              ["#","#","#",".","#","."]]
+Output: [[".","#","#"],
+         [".","#","#"],
+         ["#","#","*"],
+         ["#","*","."],
+         ["#",".","*"],
+         ["#",".","."]]
+ 
+
+Constraints:
+
+m == box.length
+n == box[i].length
+1 <= m, n <= 500
+box[i][j] is either '#', '*', or '.'.
+"""
+
+class Solution:
+    def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
+        m = len(box)
+        n = len(box[0])
+        res = [ ["." for _ in range(m)] for _ in range(n)]
+        
+        for each in range(m):
+            em = n-1
+            col = m-each-1
+            for i in range(n-1, -1, -1):
+                if box[each][i] == "*":
+                    res[i][col] = "*"
+                    em = i-1
+                elif box[each][i] == "#":
+                    res[em][col] = '#'
+                    em -= 1
+        return res
+    
